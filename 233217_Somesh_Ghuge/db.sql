435a5fa509b8e92a3853361578b08e249001547b
Somesh Ghuge <125628457+Someshghuge123@users.noreply.github.com>
2023-05-17 12:47:02 +0530

    Add files via upload

---
+++ 233217_Somesh_Ghuge/db.sql
@@ -0,0 +1,4 @@
+create table user (name varchar(20),qualification varchar(20),mob_no int primary key ,email varchar(20))
+create table job(name varcahr(20),position varchar(20),city varchar(20),
+create table hr(name varcahr(20),company varchar(20),city varchar(20),
+create table employee(name varcahr(20),comapny varchar(20),city varchar(20),vacancies int)
